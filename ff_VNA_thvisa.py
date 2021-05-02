f14108255a7fe2b098b37ef266e87ae4ca72bdca
thirschbuechler <t.hirschbuechler@gmail.com>
2021-05-02 17:22:34 +0200

    vna init untested

---
+++ ff_VNA_thvisa.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+## specific class for fieldfox VNA mode operations ##
+
+Created on Sun May 02 2021
+
+@author: thirschbuechler
+"""
+
+import fieldfox_thvisa as ff # import common functions
+import numpy as np
+import matplotlib.pyplot as plt
+
+# ToDo: 
+#- implement s2p transfer
+
+class VNA(ff.fieldfox):
+
+    # overwrite class inherited defaults
+    myprintdef = print
+    instrnamedef = "N9914A"
+    qdelaydef = 0.5
+        
+    def __init__(self, instrname = instrnamedef, qdelay, myprint = myprintdef):
+         ## set defaults or overrides as given to init() ##
+        self.timeout = 10000 # "https://pyvisa.readthedocs.io/en/1.8/resources.html#timeout"
+        self.instrname=instrname 
+        self.myprint=myprint
+        self.qdelay=qdelay
+        
+        ## call parent init ##
+        # .. righthand stuff has to be "self." properties and unusually, has no ".self" prefix
+        super(VNA, self).__init__(myprint=myprint,instrname=instrname, qdelay=qdelay) 
+
+
+
+        ## definitions ##
+        self.traces = []
+        self.abszissa = []
+        # abszissa
+        self.numPoints = 1001
+        self.startFreq = 2.4E9
+        self.stopFreq = 2.5E9
+        # further things
+        self.ifbw=1E3
+        self.avgs=1
+        self.sourcepower = "high" # "max" - bad, since ADC overload in certain ranges on S11=0dB as well as in CAL - wtf
+
+
+    def __exit__(self, exc_type, exc_value, tb):# "with" context exit: call del
+        super(VNA, self).__exit__( exc_type, exc_value, tb)
+
+
+    # enable trigger and get data into instr memory#
+    def do_sweeps(self, continous="off"):
+        # Set trigger mode to hold for trigger synchronization
+        #continous="off" # during measurement.. anyway
+        self.do_command("INIT:CONT "+str(continous)+";*OPC?")
+        
+        self.myprint("aquiring data "+str(self.avgs)+" times, acc. to avg")
+        for i in range(self.avgs): # have to manually trigger each run for the averaging..
+            ret = self.do_query_string("INIT:IMM;*OPC?")
+            self.myprint("Single Trigger complete, *OPC? returned : " + ret)
+
+
+    def make_abszissa():
+        self.abszissa = np.linspace(float(self.startFreq),float(self.stopFreq),int(self.numPoints)) #SCPI "SENSe:X?" probably unsupported
+        # however Read X-axis values possible via-     [:SENSe]:FREQuency:DATA?
+        # Assert a single trigger and wait for trigger complete via *OPC? output of a 1
+
+
+    def get_trace_mag(self, trace=1,save_trace=0):
+        self.do_command("calculate:format mlog") # default - magnitude log # calc:sel:format
+        '''
+        % Trace 1 to measurement of S21 and select that measurement as active
+        fprintf(fieldFox,'CALC:PAR1:DEF S21;SEL\n') ## does both calculate:paramater1:define s21 and calc:par1:sel
+        % Hold off for operation complete to ensure settings
+        fprintf(fieldFox,'*OPC?\n')
+        '''
+
+        self.do_command("CALC:PAR"+trace+":SEL")
+
+        ff_csv = self.do_query_string("CALC:DATA:FDATa?") # correct form was in programming examples not manual.. grumble grumble
+        
+        
+        if save_trace:
+            a=pd.DataFrame(data={"f":abszissa, "Mag":csv_array})
+            a.to_csv("trace"+trace+".csv", index=False)
+
+
+        return ff_csv.split(",")
+
+
+    def get_trace_(self, trace=1,save_trace=0):
+        self.do_command("calculate:format polar") # polar gives mag+phase
+        '''
+        % Trace 1 to measurement of S21 and select that measurement as active
+        fprintf(fieldFox,'CALC:PAR1:DEF S21;SEL\n') ## does both calculate:paramater1:define s21 and calc:par1:sel
+        % Hold off for operation complete to ensure settings
+        fprintf(fieldFox,'*OPC?\n')
+        '''
+
+        self.do_command("CALC:PAR"+trace+":SEL")
+
+        ff_csv = self.do_query_string("CALC:DATA:FDATa?") # correct form was in programming examples not manual.. grumble grumble
+                        
+        return ff_csv.split(",")
+
+    # legacy function for MAG-only (default formatting)
+    def collect_traces_mag(self):
+        for i in [1,2,3,4]:
+            self.traces.append(get_trace_mag())
+
+
+    def collect_traces(self):
+        for i in [1,2,3,4]:
+            self.traces.append(get_trace())
+
+    def plot_mag():
+        fig, ax = plt.subplots()
+        for trace in self.traces:
+            y = np.array(trace).astype(np.float)
+            ax.plot(self.abszissa,y)
+            ax.set_title (title)
+            ax.set_xlabel("Frequency (Hz)")
+            ax.set_ylabel("Magnitude (dB)")
+            
+        plt.show()
+
+
+
+#### test this library using semi Unit Testing ####
+if __name__ == '__main__': # test if called as executable, not as library, regular prints allowed
+    
+    myvna = VNA() # read IDN
+    myvna.errcheck() # because why not
+
+    myvna.ff_title("testing VNA fieldfox class")
+
+    myvna.do_sweeps()
+
+    myvna.collect_traces_mag()
+    myvna.plot_mag()
+
---
+++ fieldfox_thvisa.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+## general class for common mode independent fieldfox commands ##
+
+Created on Sun May 02 2021
+
+@author: thirschbuechler
+"""
+import time
+import struct
+import thvisa as thv
+
+
+class fieldfox(thv.thInstr):
+
+    # overwrite class inherited defaults
+    myprintdef = print
+    instrnamedef = "N9914A"
+    qdelaydef = 0.5
+        
+    def __init__(self, instrname = instrnamedef, qdelay = qdelaydef, myprint = myprintdef):
+        ## set defaults or overrides as given to init() ##
+        self.timeout = 10000 # "https://pyvisa.readthedocs.io/en/1.8/resources.html#timeout"
+        self.instrname=instrname 
+        self.myprint=myprint
+        self.qdelay=qdelay
+        
+        ## call parent init ##
+        # .. righthand stuff has to be "self." properties and unusually, has no ".self" prefix
+        super(fieldfox, self).__init__(myprint=myprint,instrname=instrname, qdelay=qdelay) 
+        
+        #self.reset() dependent on application - do in subclass or actual script
+    
+    
+    def __exit__(self, exc_type, exc_value, tb):# "with" context exit: call del
+        self.unlock()
+        self.instr.clear()
+        super(fieldfox, self).__exit__( exc_type, exc_value, tb) # self.instr.close() and so on
+
+
+    # frontpanel input access control
+    def lock(self,state=1):
+        if state:
+            self.do_command("INSTR:GTR")
+        else:
+            self.do_command("INSTR:GTL")
+    def unlock(self, state=1):
+        self.lock(not state)
+
+
+    # reset the instrument to the known default setup #
+    # same as check_instrument_errors()?
+    def errcheck():
+        myError = []
+        ErrorList = self.instr.query("SYST:ERR?").split(',')
+        Error = ErrorList[0]
+        if int(Error) == 0:
+            myprint ("+0, No Error!")
+        else:
+            while int(Error)!=0:
+                myprint ("Error #: " + ErrorList[0])
+                myprint ("Error Description: " + ErrorList[1])
+                myError.append(ErrorList[0])
+                myError.append(ErrorList[1])
+                ErrorList = self.instr.query("SYST:ERR?").split(',')
+                Error = ErrorList[0]
+                myError = list(myError)
+        return myError
+
+
+    
+    # $ test invinivision stuff
+    # $$ what happens to cal?
+    # alternatively, MMEMory:LOAD:STATe "autosave1.sta"
+    # or something like that
+    # store instrument setup state in script dir #
+    def store_setup(self, filename="setup.sta"):
+        #sSetup =self.do_query_ieee_block(":SYSTem:SETup?")
+        #f = open(filename, "wb")
+        #f.write(sSetup)
+        #f.close()
+        #self.myprint("Setup bytes saved: %d" % len(sSetup))
+        pass
+
+
+    # load instrument setup state from script dir #
+    def load_setup(self, filename="setup.sta"):
+        #f = open(filename, "rb")
+        #sSetup = f.read()
+        #f.close()
+        #self.do_command_ieee_block(":SYSTem:SETup", sSetup)
+        #self.myprint("Setup bytes restored: %d" % len(sSetup))
+        pass
+
+    
+
+    # enable trigger and get data #
+    #def capture(self): # mode depentent unfortunately, see ff_vna (exists) and ff_speccy (todo)
+        #pass
+        
+
+    def ff_title(self, title):
+        self.do_command("DISPlay:TITLe:DATA "+title)
+
+
+
+#### test this library using semi Unit Testing ####
+if __name__ == '__main__': # test if called as executable, not as library, regular prints allowed
+    
+    myff = fieldfox() # read IDN
+    myff.errcheck() # because why not
+
+    myff.ff_title("testing general fieldfox class")
+ 
