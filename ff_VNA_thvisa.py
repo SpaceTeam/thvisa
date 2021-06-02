36098a227043d8aa43fdc798c91e4f7f87a20dca
thirschbuechler <thirschbuechler@github.com>
2021-06-02 14:27:27 +0200

    state and cal readout

--- ff_VNA_thvisa.py
+++ ff_VNA_thvisa.py
@@ -20,7 +20,8 @@ class VNA(ff.fieldfox):
 
     # overwrite class inherited defaults
     myprintdef = print
-    instrnamedef = "TCPIP::K-N9914A-71670.local::inst0::INSTR" # full name since no autodetection 
+    instrnamedef = "TCPIP::K-N9914A-71670.local::inst0::INSTR" # TCPIP: full name since no autodetection 
+    instrnamedef = "USB0::10893::23576::MY57271670::0::INSTR" # USB: not fullname required but whatever
     
         
     def __init__(self, instrname = instrnamedef, myprint = myprintdef, qdelay = 0, wdelay=0):
@@ -46,21 +47,22 @@ class VNA(ff.fieldfox):
         super(VNA, self).__exit__( exc_type, exc_value, tb) # call parent
 
 
-    def setup(self):
-        # Preset the FieldFox  
-        self.do_command("SYST:PRES")#"SYST:PRES;*OPC?" # docommand does opc inherited by fieldfox mainclass
-        # Set mode to VNA   
-        self.do_command("INST:SEL 'NA'")
-        
+    def setup(self, hard=True, numPoints = 1001, startFreq = 2.4E9, stopFreq = 2.5E9, ifbw=1E3, avgs=1, sourcepower = "high" ):
+        if hard:
+            # Preset the FieldFox  
+            self.do_command("SYST:PRES")#"SYST:PRES;*OPC?" # docommand does opc inherited by fieldfox mainclass
+            # Set mode to VNA   
+            self.do_command("INST:SEL 'NA'")
+            
                 
         # abscissa
-        self.numPoints = 1001
-        self.startFreq = 2.4E9
-        self.stopFreq = 2.5E9
+        self.numPoints = numPoints
+        self.startFreq = startFreq
+        self.stopFreq = stopFreq
         # further things
-        self.ifbw=1E3
-        self.avgs=1
-        self.sourcepower = "high" # "max" - bad, since ADC overload in certain ranges on S11=0dB as well as in CAL - wtf
+        self.ifbw=ifbw
+        self.avgs=avgs
+        self.sourcepower = sourcepower
         
         
         ## msr setup ##
@@ -68,11 +70,38 @@ class VNA(ff.fieldfox):
         self.do_command("SENS:FREQ:START " + str(self.startFreq))
         self.do_command("SENS:FREQ:STOP " + str(self.stopFreq))
         self.do_command("BWID " + str(self.ifbw))
-        self.do_command("source:power " + str(self.sourcepower))
         self.do_command("AVER:COUNt " + str(self.avgs))
         
+        if self.sourcepower=="high":
+            self.do_command("SOUR:POW:ALC HIGH")#autolevel high
+        elif self.sourcepower=="low":
+            self.do_command("SOUR:POW:ALC low")#autolevel low
+        else:
+            raise Exception("manual sourcepower setting is not implemented and discouraged")
+            # e.g. "max" is bad, since ADC overload in certain ranges on S11=0dB as well as in CAL - wtf
+            #self.do_command("source:power " + str(self.sourcepower))#for manual control only
+        
         self.setup_done = True
-
+        
+    def askandlog(self, thing):
+        return(thing+" "+self.do_query_string(thing))
+            
+        
+    def query_setup(self):
+        
+        log=[]
+        log.append(self.askandlog("SENS:SWE:POIN?"))
+        log.append(self.askandlog("SENS:FREQ:START?"))
+        log.append(self.askandlog("SENS:FREQ:STOP?"))
+        log.append(self.askandlog("BWID?"))
+        log.append(self.askandlog("AVER:COUNt?"))
+        log.append(self.askandlog("SOUR:POW:ALC?"))
+        log.append(self.cal_str())
+                        
+        return log
+        
+            
+            
 
     # enable trigger and get data into instr memory
     def do_sweeps(self, continous="off"):
@@ -98,7 +127,14 @@ class VNA(ff.fieldfox):
             #SCPI "SENSe:X?" probably unsupported
             # however Read X-axis values possible via-     [:SENSe]:FREQuency:DATA?
 
-
+    
+    def logmag_all(self):# for cal and so on
+        for i in [1,2,3,4]:
+            self.do_command("CALC:PAR"+str(i)+":SEL") # select trace
+            self.do_command("calculate:format Mlog") # set logmag
+            
+            
+    
     def get_trace(self, trace=1):
         self.do_command("CALC:PAR"+str(trace)+":SEL") # select trace
         smiths = [1,4]
@@ -145,9 +181,53 @@ class VNA(ff.fieldfox):
         s2p_frame.to_csv(filename, index=False, sep ='\t', header=False) # save
         # notes:    abscissa is column not index so ignore index
         #           header is columname overwrite
+        self.myprint("saved "+filename)
     
+    
+    def is_cal_d(self):
+        res=self.do_query_string("sense:correction:user:state?")
+        if int(res):#cast to int
+            return 1
+        else:
+            return 0
 
 
+    def cal_str(self):
+        return ("usercal is done?: "+str(self.is_cal_d()))
+
+
+    def save_cal(self, prefix="", suffix=""):
+    # Set and read error term data [:SENSe]:CORRection:COEFficient[:DATA]
+    # http://ena.support.keysight.com/e5071c/manuals/webhelp/eng/programming/command_reference/sense/scpi_sense_ch_correction_coefficient_data.htm
+    
+        # common definitions
+        fields = ["ES",#: Source match
+                  "ER",#: Reflection tracking
+                  "ED",#: Directivity
+                  "EL",#: Load match
+                  "ET"]#: Transmission tracking]
+        
+        Sparams=["11","22","21","12"]
+        
+        for Sparam in Sparams:
+            # local definitions
+            res=[]
+            filename=prefix+"errterms_"+Sparam+suffix+".csv"
+            rx_p=str(Sparam[1])
+            tx_p=str(Sparam[0])
+            
+            # get cal
+            for field in fields:
+                res.append(self.do_query_string(("SENS1:CORR:COEF? "+field+","+rx_p+","+tx_p)).split(","))
+            
+            # save cal
+            csv=pd.DataFrame.from_records(res).transpose()#from_records takes it into columns so transpose
+            csv.columns=fields
+            csv.to_csv(filename)
+        
+        
+        
+    
 
 #### test this library using semi Unit Testing ####
 if __name__ == '__main__': # test if called as executable, not as library, regular prints allowed
@@ -163,9 +243,11 @@ if __name__ == '__main__': # test if called as executable, not as library, regul
         t2 = perf_counter()
 
         myvna.collect_traces()
-        myvna.save_csv("bb.s2p")
+        myvna.save_csv("dmu75-parkedleft-called_on-air_closeddoor.s2p")
         t3 = perf_counter()
         
         print("took {:.2f}s for sweeping, {:.2f}s for fetch 'n save".format(t2-t1,t3-t2))
         
-        myvna.ff_title()
+        myvna.ff_title(myvna.cal_str())#empty titlebar
+        
+        print(myvna.query_setup())
